45570cb89f7910f11ca6d032092cf599f416630c
PETER PHIRI <swadamonia@gmail.com>
2023-02-01 17:18:12 +0200

    Olympus.update.appjs
    two codes to be inherited

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,4 @@ import '../global.css'
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+code default browser to be updated on any background that sloc
